48d71360991bc0b98299c7d594ea7958d16b03ae
Alejandro Ciniglio <mail@alejandrociniglio.com>
2014-10-04 11:41:59 -0400

    initial

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to nlangs
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/nlangs/core_test.clj
@@ -0,0 +1,7 @@
+(ns nlangs.core-test
+  (:require [clojure.test :refer :all]
+            [nlangs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
