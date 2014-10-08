c9031bd63dc770d282efac258f5076d61b87c3a6
Alejandro Ciniglio <mail@alejandrociniglio.com>
2014-10-08 18:21:50 -0400

    after presentation

---
+++ support/compojure.clj
@@ -0,0 +1,28 @@
+(ns nlangs.compojure
+  (:require [compojure.core :refer :all]
+            [compojure.route :refer [resources]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.json :refer [wrap-json-response
+                                          wrap-json-params
+                                          wrap-json-body]]
+            [nlangs.sql-api :as api]))
+
+(defroutes app
+  (GET "/" [] "<h1>Hello World</h1>")
+  (GET "/grocery-list" [] {:body (api/grocery-list)})
+  (POST "/grocery-list" [name]
+        {:body (api/add-to-grocery-list name)})
+  (PUT "/grocery-list" [name bought]
+       {:body (api/update-item-bought name bought)})
+  (resources "/"))
+
+(def server
+  (run-jetty
+   (wrap-json-params
+    (wrap-json-response app))
+   {:port 3000 :join? false}))
+
+(defn stop [] (.stop server))
+
+;; make the request
+(stop)
